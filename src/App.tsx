cad52f88a22b4a2c1bc7a416890b823a5a535022
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2023-02-23 15:21:37 +0300

    feat/MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles/App.css";
-import { Button } from "./ui/Button";
-import { Input } from "./ui/Input";
-import { Checkbox } from "./ui/Checkbox";
+import { MainPage } from "./pages/MainPage";
 
 function App() {
-  const [email, setEmail] = useState("faruk");
-  const [checked, setChecked] = useState(false);
-  return (
-    <div className="bg-bgColor flex flex-col h-screen">
-      <Button size="xlarge" bending="high">
-        faruk
-      </Button>
-      <br />
-      <br />
-      <div className="flex justify-center">
-        <div className="w-1/4">
-          <Input
-            title={"Eye"}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            icon={"mailbox"}
-          />
-          <br />
-          <Input
-            title={"Mail"}
-            value={email}
-            disabled={true}
-            onChange={(e) => setEmail(e.target.value)}
-            icon={"eyeOff"}
-          />
-        </div>
-      </div>
-      <br />
-      <br />
-      <div className="self-center">
-        <Checkbox checked={checked} onClick={() => setChecked(!checked)} />
-      </div>
-    </div>
-  );
+  return <MainPage />;
 }
 
 export default App;
