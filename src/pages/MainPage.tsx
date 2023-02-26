cf18133c981b65a5c420cdf3f9703b23c85b5f3c
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2023-02-26 23:04:43 +0300

    feat/WebMainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,62 +1,23 @@
-import React, { useState } from "react";
-import { Input } from "../ui/Input";
-import { Checkbox } from "../ui/Checkbox";
+import React from "react";
 import { Button } from "../ui/Button";
 
 export function MainPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
-  const [checked, setChecked] = useState(false);
   return (
-    <div className="bg-bgColor h-screen items-center flex flex-col">
-      <div className="w-full mt-16 flex flex-col items-center">
+    <div className="bg-bgColor h-screen flex flex-col">
+      <div className="flex flex-row items-center mt-16 ml-12">
         <img
           src={require("../assets/images/LOGO.png")}
-          width={192}
-          height={192}
-          alt="treasure_logo"
+          alt="logo"
+          width={186}
+          height={140}
         />
-        <div>
-          <div className="w-80 mt-12">
-            <Input
-              title="Email"
-              icon="mailbox"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div className="w-80 mt-4">
-            <Input
-              title="Password"
-              icon={isPasswordVisible ? "eyeOn" : "eyeOff"}
-              value={password}
-              type={!isPasswordVisible ? "password" : undefined}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              onIconClick={() => {
-                setIsPasswordVisible(!isPasswordVisible);
-              }}
-            />
-          </div>
-          <div className="mt-4 flex flex-row items-center">
-            <Checkbox
-              checked={checked}
-              onClick={() => {
-                setChecked(!checked);
-              }}
-            />
-            <p className="text-white ml-2">Remember Me</p>
-          </div>
-          <div className="flex justify-center">
-            <div className="mt-4 w-40">
-              <Button size="xlarge">Login</Button>
-            </div>
-          </div>
-        </div>
+        <p className="text-white text-5xl ml-12">Welcome Alp Kartal</p>
+      </div>
+      <div className="w-52 mt-20 ml-12">
+        <Button size="xlarge">Create Treasure</Button>
+      </div>
+      <div className="w-52 mt-8 ml-12">
+        <Button size="xlarge">Created Treasures</Button>
       </div>
     </div>
   );
